db7f282ad99c8ad4420a775b6ab7de37bdc42917
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2024-11-04 18:29:59 +0500

    contstruction section and tabs section fixed

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+const tabsList = document.querySelectorAll(".ap-tab-list li");
+const appTabContainer = document.querySelector(".ap-tab-row");
+const cards = document.querySelectorAll(".card");
+
+// Функция для отображения карточек по категории
+function displayCards(dataCategory) {
+  // Создаем новый контейнер .row для размещения карточек
+  const row = document.createElement("div");
+  row.classList.add("row");
+
+  // Фильтруем карточки по категории
+  let filteredCards;
+  if (dataCategory === "all") {
+    filteredCards = Array.from(cards);
+  } else {
+    filteredCards = Array.from(cards).filter((card) => {
+      return card.getAttribute("data-category") === dataCategory;
+    });
+  }
+
+  // Вставляем отфильтрованные карточки в контейнер .row
+  filteredCards.forEach((card) => {
+    row.appendChild(card.parentNode);
+  });
+
+  // Очищаем appTabContainer и добавляем новый .row с карточками
+  appTabContainer.innerHTML = "";
+  appTabContainer.appendChild(row);
+}
+
+// Событие клика по вкладкам
+tabsList.forEach((tabButton) => {
+  tabButton.addEventListener("click", (e) => {
+    const dataCategory = e.target.getAttribute("data-category");
+    console.log(dataCategory);
+
+    tabsList.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    // Сохраняем выбранную категорию в localStorage
+    localStorage.setItem("selectedCategory", dataCategory);
+
+    // Отображаем карточки по выбранной категории
+    displayCards(dataCategory);
+  });
+});
+
+// Получаем сохраненную категорию при загрузке страницы
+const savedCategory = localStorage.getItem("selectedCategory") || "all";
+
+// Устанавливаем активную вкладку и отображаем карточки
+tabsList.forEach((tab) => {
+  if (tab.getAttribute("data-category") === savedCategory) {
+    tab.classList.add("active");
+  }
+});
+displayCards(savedCategory);
